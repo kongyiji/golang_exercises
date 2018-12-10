cb5351ceac66560a5622ba651997a54822af340d
kongyiji <xuyinghua1000@gmail.com>
2018-12-10 21:36:11 +0800

    d4

--- src/map/exercise-maps.go
+++ src/map/exercise-maps.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 
 	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
-	// length := len(s)
-	countMap = make(map[string]int)
+	countMap := make(map[string]int)
 	wordList := strings.Fields(s)
 	l := len(wordList)
 	for i := 0; i < l; i++ {
-
+		countMap[wordList[i]]++
 	}
-	fmt.Printf("%d, %q\n", l, wordList)
-	return map[string]int{"x": 1}
+	return countMap
 }
 
 func main() {
